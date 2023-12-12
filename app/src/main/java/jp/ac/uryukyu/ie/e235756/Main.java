a95f29a2cdb2fc41c208dea04cc50e9a516d3506
koba73 <e235756@ie.u-ryukyu.ac.jp>
2023-12-12 09:37:27 +0900

    1st commit

--- app/src/main/java/jp/ac/uryukyu/ie/e235756/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235756/Main.java
@@ -1,5 +1,9 @@
 package jp.ac.uryukyu.ie.e235756;
 
 public class Main {
-    
+    public static void main(String[] args){
+    String str = null;
+    System.out.println(str.length());
+
+    }
 }

de80468badf4fca9490de235e7edd6046c08ad18
koba73 <e235756@ie.u-ryukyu.ac.jp>
2023-12-12 09:53:35 +0900

    2nd commit

--- app/src/main/java/jp/ac/uryukyu/ie/e235756/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235756/Main.java
@@ -2,8 +2,12 @@ package jp.ac.uryukyu.ie.e235756;
 
 public class Main {
     public static void main(String[] args){
-    String str = null;
-    System.out.println(str.length());
-
+        String str=null;
+        try{
+            System.out.println(str.length());
+        }catch(java.lang.NullPointerException e){
+            System.out.println("java.lang.NullPointerExceptionが発生しました。");
+            System.out.println(e.getMessage());
+        }
     }
 }
